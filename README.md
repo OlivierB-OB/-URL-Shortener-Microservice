1eb075d6bad1b990bc95b23fa2eb9705b1ace499
Olivier BERNARD <olivier.bernard.ob@gmail.com>
2016-03-18 13:55:30 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+Olivier BERNARD 's URL Shortener Microservice
+===
+
+User stories:
+
+1.  I can pass a URL as a parameter and I will receive a shortened URL in the JSON response.
+1.  I pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain an error instead.
+1.  When I visit that shortened URL, it will redirect me to my original link.
+
+Example usage:
+
+*  [https://ob-url-shortener-microservice/new/https://www.google.com](https://ob-url-shortener-microservice/new/https://www.google.com)
+*  [https://ob-url-shortener-microservice/1](https://ob-url-shortener-microservice/1)
+
+Example output:
+
+*  { "original_url": "https://www.google.com", "short_url": "https://ob-url-shortener-microservice/1" }
+*  { "error": "Invalid URL provided!" }
---
+++ server.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var fs = require('fs');
+var pg = require('pg');
+var validUrl = require('valid-url');
+
+var app = express();
+pg.defaults.ssl = true;
+
+app.get('/', home);
+app.get('/new/:uri*', create);
+app.get('/:id', use);
+
+app.listen(process.env.PORT, function () {
+    console.log('Example app listening on port :{1}!'.replace('{1}', process.env.PORT));
+});
+
+// ====================================================
+
+function home (req, res) {
+    res.send(fs.readFileSync('home.html', 'utf8'));
+}
+
+function create (req, res) {
+    if (validUrl.isUri(req.params.uri)){
+        res.send({ "error": "Invalid URL provided!" });
+    } else {
+        pg.connect(process.env.DATABASE_URL, function(err, client) {
+            if (err) throw err;
+            client.query("insert into urlminification (url) values ('{1}') returning id;".replace('{1}', req.params.uri))
+            .on('row', function(row) {
+                res.send({ "original_url": req.params.uri, "short_url": "https://ob-url-shortener-microservice/" + row[0] });
+            });
+        });
+    }
+}
+
+function use (req, res) {
+    pg.connect(process.env.DATABASE_URL, function(err, client) {
+        if (err) throw err;
+        client.query('select url from urlminification where id = {1};'.replace(req.params.id))
+        .on('row', function(row) {
+            res.redirect(row[0]);
+        });
+    });
+}
